c69ea5ecff8700162e9f23de5f0dda3738746e02
DaRealFreak <steffen.keuper@web.de>
2020-01-01 23:32:25 +0100

    [TASK] add unit tests for client with HTTP and SOCK5 proxy transport layers

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/EDDYCJY/fake-useragent v0.2.0
 	github.com/PuerkitoBio/goquery v1.5.0 // indirect
 	github.com/stretchr/testify v1.4.0
+	golang.org/x/net v0.0.0-20181114220301-adae6a3d119a
 )
--- round_tripper_test.go
+++ round_tripper_test.go
@@ -1,10 +1,14 @@
 package cloudflarebp
 
 import (
+	"fmt"
 	"net/http"
+	"net/url"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/proxy"
 )
 
 func TestApplyCloudFlareByPassDefaultClient(t *testing.T) {
@@ -25,6 +29,57 @@ func TestApplyCloudFlareByPassDefaultClient(t *testing.T) {
 func TestApplyCloudFlareByPassDefinedTransport(t *testing.T) {
 	client := &http.Client{
 		Transport: &http.Transport{},
+	}
+
+	// if the client requests something before applying the fix some configurations are applied already
+	// and our ByPass won't work anymore, so we have to apply our ByPass as the first thing
+	client.Transport = AddCloudFlareByPass(client.Transport)
+
+	res, err := client.Get("https://www.patreon.com/login")
+	assert.New(t).NoError(err)
+	assert.New(t).Equal(200, res.StatusCode)
+}
+
+// TestAddCloudFlareByPassSocksProxy tests the CloudFlare bypass while we're using a SOCK5 proxy transport layer
+func TestAddCloudFlareByPassSocksProxy(t *testing.T) {
+	auth := proxy.Auth{
+		User:     os.Getenv("PROXY_USER"),
+		Password: os.Getenv("PROXY_PASS"),
+	}
+
+	dialer, err := proxy.SOCKS5(
+		"tcp",
+		fmt.Sprintf("%s:1080", os.Getenv("PROXY_HOST")),
+		&auth,
+		proxy.Direct,
+	)
+	assert.New(t).NoError(err)
+
+	client := &http.Client{
+		Transport: &http.Transport{Dial: dialer.Dial},
+	}
+
+	// if the client requests something before applying the fix some configurations are applied already
+	// and our ByPass won't work anymore, so we have to apply our ByPass as the first thing
+	client.Transport = AddCloudFlareByPass(client.Transport)
+
+	res, err := client.Get("https://www.patreon.com/login")
+	assert.New(t).NoError(err)
+	assert.New(t).Equal(200, res.StatusCode)
+}
+
+// TestAddCloudFlareByPassHTTPProxy tests the CloudFlare bypass while we're using a HTTP proxy transport layer
+func TestAddCloudFlareByPassHTTPProxy(t *testing.T) {
+	proxyURL, _ := url.Parse(
+		fmt.Sprintf(
+			"http://%s:%s@%s:80",
+			url.QueryEscape(os.Getenv("PROXY_USER")), url.QueryEscape(os.Getenv("PROXY_PASS")),
+			url.QueryEscape(os.Getenv("PROXY_HOST")),
+		),
+	)
+
+	client := &http.Client{
+		Transport: &http.Transport{Proxy: http.ProxyURL(proxyURL)},
 	}
 
 	// if the client requests something before applying the fix some configurations are applied already
